8fc89b20cb0f64f39148f8780ebeee9b97374c65
Mate Balo Bordas <mate.balob@gmail.com>
2016-07-06 16:15:47 +0200

    export schemas in index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export let draft4 = {
+    cltSchema: require('./parser/schemas/draft-4/CLT-schema.json'),
+    wfSchema: require('./parser/schemas/draft-4/WF-schema.json'),
+    etSchema: require('./parser/schemas/draft-4/ET-schema.json')
+};
+
+export let draft3 = {
+    cltSchema: require('./parser/schemas/draft-3/CLT-schema.json'),
+    wfSchema: require('./parser/schemas/draft-3/WF-schema.json'),
+    etSchema: require('./parser/schemas/draft-3/ET-schema.json')
+};
