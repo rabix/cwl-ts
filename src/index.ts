313f728c8e09151d6feec8b9d28a1dd4a2a0ac68
Maja Nedeljković <maya.nedeljko@gmail.com>
2016-07-13 19:32:38 +0200

    Moved model from draft-4 to v1.

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ declare function require(name:string);
 export let draft4 = {
     cltSchema: require('./parser/schemas/draft-4/CLT-schema.json'),
     wfSchema: require('./parser/schemas/draft-4/WF-schema.json'),
-    etSchema: require('./parser/schemas/draft-4/ET-schema.json')
+    etSchema: require('./parser/schemas/draft-4/ET-schema.json'),
+    model: {
+        CommandLineToolModel: CommandLineToolModel
+    }
 };
 
 export let draft3 = {
     cltSchema: require('./parser/schemas/draft-3/CLT-schema.json'),
     wfSchema: require('./parser/schemas/draft-3/WF-schema.json'),
-    etSchema: require('./parser/schemas/draft-3/ET-schema.json'),
-    model: {
-        CommandLineToolModel: CommandLineToolModel
-    }
+    etSchema: require('./parser/schemas/draft-3/ET-schema.json')
 };
