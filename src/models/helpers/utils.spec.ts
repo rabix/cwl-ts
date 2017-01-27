3289b6415daf3ed55aca0e64d03928ab22349d7f
Maja Nedeljković <maya.nedeljko@gmail.com>
2017-01-27 14:56:30 +0100

    Utility functions for transforming cwl-style maps to cwl-style lists

---
+++ src/models/helpers/utils.spec.ts
@@ -0,0 +1,113 @@
+import {expect} from "chai";
+import {ensureArray, checkMapValueType} from "./utils";
+
+describe("ensureArray", () => {
+    it("should return an array from a map of objects", () => {
+        const test = {
+            foo: {d: "a"},
+            bar: {a: "q"}
+        };
+        const arr = ensureArray(test, "class");
+
+        expect(arr).to.not.be.empty;
+        expect(arr).to.have.length(2);
+
+        arr.forEach(i => {
+            expect(i).to.haveOwnProperty("class");
+        });
+    });
+
+    it("should return original array", () => {
+        const test = [1, 2, 4];
+        const arr = ensureArray(test, "foo");
+
+        expect(arr).to.deep.equal(test);
+    });
+
+    it("should set inner property if objects are primitives", () => {
+        const test = {
+            foo: "hello",
+            bar: "world"
+        };
+        const arr = ensureArray(test, "class", "type");
+
+        expect(arr).to.not.be.empty;
+        expect(arr).to.have.length(2);
+
+        arr.forEach(i => {
+            expect(i).to.haveOwnProperty("class");
+            expect(i).to.haveOwnProperty("type");
+        });
+
+        expect(arr[0]["class"]).to.equal("foo");
+        expect(arr[1]["class"]).to.equal("bar");
+
+        expect(arr[0]["type"]).to.equal("hello");
+        expect(arr[1]["type"]).to.equal("world");
+    });
+});
+
+describe("checkMapValueType", () => {
+    it("should identify map of strings", () => {
+        const test = {
+            foo: "bar",
+            baz: "maz"
+        };
+
+        expect(checkMapValueType(test)).to.equal("string");
+    });
+
+    it("should identify map of numbers", () => {
+        const test = {
+            foo: 3,
+            baz: 4
+        };
+
+        expect(checkMapValueType(test)).to.equal("number");
+    });
+
+    it("should identify map of objects", () => {
+        const test = {
+            foo: {},
+            baz: {}
+        };
+
+        expect(checkMapValueType(test)).to.equal("object");
+    });
+
+    it("should identify map of nulls", () => {
+        const test = {
+            foo: null,
+            baz: null
+        };
+
+        expect(checkMapValueType(test)).to.equal("null");
+    });
+
+    it("should identify map of undefineds", () => {
+        const test = {
+            foo: undefined,
+            baz: undefined
+        };
+
+        expect(checkMapValueType(test)).to.equal("undefined");
+    });
+
+    it("should identify map of booleans", () => {
+        const test = {
+            foo: true,
+            baz: false
+        };
+
+        expect(checkMapValueType(test)).to.equal("boolean");
+    });
+
+    it("should identify map of mismatched types", () => {
+        const test = {
+            foo: true,
+            baz: 3
+        };
+
+        expect(checkMapValueType(test)).to.equal("mismatch");
+    });
+});
---
+++ src/models/helpers/utils.ts
@@ -0,0 +1,44 @@
+export const ensureArray = (map: {[key: string]: any} | any[], key: string, valueKey?: string): any[] => {
+    if (Array.isArray(map)) return map;
+
+    if (!map) return [];
+
+    return Object.keys(map).map(prop => {
+        if (valueKey && checkMapValueType(map) !== "object") {
+            return {... {[valueKey]: map[prop]},...{[key]: prop}};
+        }
+        return {... map[prop], ...{[key]: prop}};
+    });
+};
+
+export const checkMapValueType = (map: {[key: string]: any}): "string" | "number" | "undefined" | "object" | "array" | "null" | "mismatch" => {
+    let type = null;
+
+    Object.keys(map).forEach((key) => {
+        let valType;
+        const value = map[key];
+
+        if (Array.isArray(value)) {
+            valType = "array";
+        } else if (value === null) {
+            valType = "null";
+        } else if (typeof value === "object") {
+            valType = "object";
+        } else {
+            valType = typeof value;
+        }
+
+        if (type && valType !== type) {
+            type = "mismatch";
+            return type;
+        } else {
+            type = valType;
+        }
+    });
+
+    return type;
+};
+
+export const convertToObject = (item: any, key: string): {[key: string]: any} => {
+    return {[key]: item};
+};
