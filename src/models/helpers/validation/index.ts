7aa62e56da9b0569716c58456a82c757099d2b13
Maja Nedeljković <maya.nedeljko@gmail.com>
2017-10-12 14:31:41 +0200

    Removed unused imports

--- src/models/helpers/validation/index.ts
+++ src/models/helpers/validation/index.ts
@@ -1,6 +1,5 @@
 export * from "./Issue";
 export * from "./Validation";
 export * from "./ValidationError";
-export * from "./Validatable";
 export * from "./ValidationBase";
 export * from "./ValidationUpdate";
