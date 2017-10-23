881f8ac45a93b3815bf4d9f7704d58b5f3af364e
Maja Nedeljković <maya.nedeljko@gmail.com>
2017-10-23 12:52:13 +0200

    remove extra functions from expression lib

---
+++ src/models/helpers/sbg-expression-lib.ts
@@ -0,0 +1,27 @@
+export const sbgHelperLibrary = `
+var inheritMetadata = function(o1, o2) {
+    var commonMetadata = {};
+    if (!Array.isArray(o2)) {
+        o2 = [o2]
+    }
+    for (var i = 0; i < o2.length; i++) {
+        var example = o2[i]['metadata'];
+        for (var key in example) {
+            if (i == 0)
+                commonMetadata[key] = example[key];
+            else {
+                if (!(commonMetadata[key] == example[key])) {
+                    delete commonMetadata[key]
+                }
+            }
+        }
+    }
+    if (!Array.isArray(o1)) {
+        o1 = setMetadata(o1, commonMetadata)
+    } else {
+        for (var i = 0; i < o1.length; i++) {
+            o1[i] = setMetadata(o1[i], commonMetadata)
+        }
+    }
+    return o1;
+};`;
