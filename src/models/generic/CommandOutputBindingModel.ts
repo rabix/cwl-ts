cc0a0d6e2e861a490ec823d32f9d6290e7c84be9
milosdanilov <milosdanilov@gmail.com>
2021-01-21 18:24:26 +0100

    feat(CommandOutputBinding): support Array<string> for glob

--- src/models/generic/CommandOutputBindingModel.ts
+++ src/models/generic/CommandOutputBindingModel.ts
@@ -14,9 +14,9 @@ export abstract class CommandOutputBindingModel extends ValidationBase implement
 
     secondaryFiles: ExpressionModel[];
 
-    glob: ExpressionModel;
+    glob: ExpressionModel | Array<string>;
 
-    protected _glob: ExpressionModel;
+    protected _glob: ExpressionModel | Array<string>;
 
     loadContents: boolean;
 
@@ -36,27 +36,21 @@ export abstract class CommandOutputBindingModel extends ValidationBase implement
 
     abstract deserialize(attr: any): void;
 
-    protected setGlob(value: ExpressionModel, exprConstructor: new (...args: any[]) => ExpressionModel) {
-        let val = value.serialize();
-        this._glob.clearIssue(ErrorCode.ALL);
-        this._glob = new exprConstructor(val, `${this.loc}.glob`, this.eventHub);
-        this._glob.setValidationCallback(err => this.updateValidity(err));
-        this.validateGlob();
-    }
-
     protected validateGlob() {
         if (!this._glob) return;
 
-        if (this._glob.serialize() === undefined) {
-            this._glob.setIssue({
-                [`${this.loc}.glob`]: {
-                    message: "Glob should be specified",
-                    type: "warning",
-                    code: ErrorCode.OUTPUT_GLOB_MISSING
-                }
-            }, true);
-        } else {
-            this._glob.clearIssue(ErrorCode.OUTPUT_GLOB_MISSING);
+        if (this._glob instanceof ExpressionModel) {
+            if (this._glob.serialize() === undefined) {
+                this._glob.setIssue({
+                    [`${this.loc}.glob`]: {
+                        message: "Glob should be specified",
+                        type: "warning",
+                        code: ErrorCode.OUTPUT_GLOB_MISSING
+                    }
+                }, true);
+            } else {
+                this._glob.clearIssue(ErrorCode.OUTPUT_GLOB_MISSING);
+            }
         }
     }
 
@@ -67,4 +61,15 @@ export abstract class CommandOutputBindingModel extends ValidationBase implement
         this._outputEval = new exprConstructor(value.serialize(), `${this.loc}.outputEval`, this.eventHub);
         this._outputEval.setValidationCallback(err => this.updateValidity(err));
     }
+
+    protected setGlobExpression(expression: ExpressionModel, exprConstructor: new (...args: any[]) => ExpressionModel) {
+        if (this._glob instanceof ExpressionModel) {
+            this._glob.clearIssue(ErrorCode.ALL);
+        }
+
+        this._glob = new exprConstructor(expression.serialize(), `${this.loc}.glob`, this.eventHub);
+        this._glob.setValidationCallback(err => this.updateValidity(err));
+        this.validateGlob();
+    }
+
 }
